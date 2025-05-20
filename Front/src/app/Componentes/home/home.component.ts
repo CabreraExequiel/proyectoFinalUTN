bf131296e47a1e98616a16313407df2fcc20027d
Lucas Mieres <lucassmieres2001@gmail.com>
2025-05-19 21:05:48 -0300

    avances

--- Front/src/app/Componentes/home/home.component.ts
+++ Front/src/app/Componentes/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UsuarioComponent } from "../usuario/usuario.component";
+import { FootballComponent } from '../football/football.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [UsuarioComponent],
+  imports: [UsuarioComponent,FootballComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
