d1e42e17ef300b10500a06f5a1ff1c93ed6b8879
Bruno Fredes <brunofredes99@gmail.com>
2025-06-06 21:43:12 -0300

    cambio_perfil

--- Front/src/polyfills.ts
+++ Front/src/polyfills.ts
@@ -3,5 +3,5 @@
 import { Buffer } from 'buffer';
 (window as any).Buffer = Buffer;
 
-import * as process from 'process';
-(window as any).process = process;
+//import * as process from 'process';
+//(window as any).process = process;
